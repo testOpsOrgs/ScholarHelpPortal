2740a132ea620fe2fd26e0d9d646ee8a3cf7005c
rithik609609 <63301333+rithik609609@users.noreply.github.com>
2023-11-17 13:01:29 +0530

    Subject Page

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './views/Home';
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Admin from './views/Admin';
 import Year from './views/Year';
 import TwoYearPage from './views/TwoYearPage';
 import FourYearPage from './views/FourYearPage';
 import Login from './views/Login';
-
+import SemesterSubjects from './views/SemesterSubjects';
 function App() {
   return (
     <Routes>
-      <Route strict exact path ="/" element={<Home/>}/>
-      <Route strict exact path='/Year' element={<Year/>}/>
-      <Route strict exact path='/Admin' element={<Admin/>}/>
-      <Route strict exact path='/2Y' element={<TwoYearPage/>}/>
-      <Route strict exact path='/4Y' element={<FourYearPage/>}/>
-      <Route strict exact path='/Login' element={<Login/>}/>
+      <Route strict exact path="/" element={<Home />} />
+      <Route strict exact path='/Year' element={<Year />} />
+      <Route strict exact path='/Admin' element={<Admin />} />
+      <Route strict exact path='/2Y' element={<TwoYearPage />} />
+      <Route strict exact path='/4Y' element={<FourYearPage />} />
+      <Route strict exact path='/Login' element={<Login />} />
+      <Route strict exact path="/semester/:year/:semester" element={<SemesterSubjects />}/>
     </Routes>
   );
 }
--- UI/src/views/TwoYearPage.js
+++ UI/src/views/TwoYearPage.js
@@ -1,20 +1,60 @@
+
+// Import your components
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import '../views/TwoYearPage.css'
+import SemesterSubjects from './SemesterSubjects';
+import { useNavigate } from 'react-router-dom';
 
-export default function TwoYearPage(){
-	return(
+export default function TwoYearPage() {
+	// Your subject data
+	
+
+	const navigate = useNavigate();
+	const year = "year2";
+
+	const handleSemesterClick = (semester) => {
+		navigate(`/semester/${year}/${semester}`);
+	};
+
+	return (
+
 		<div>
-			<Header/>
+			<Header />
 			<div className="main-container2Y">
-				<div className="square"> 1</div>
-				<div className="square"> 2</div>
-				<div className="square"> 3</div>
-				<div className="square"> 4</div>
-			</div>
-			<Footer/>
 
+
+				<div className="square2Y" onClick={() => handleSemesterClick('sem1')}>
+					<h2>Sem 1</h2>
+					<p>Nulla fugiat consectetur do proident fugiat duis duis aute sint pariatur aute ex.</p>
+				</div>
+
+
+				<div className="square2Y" onClick={() => handleSemesterClick('sem2')}>
+					<h2>Sem 2</h2>
+					<p>Nulla fugiat consectetur do proident fugiat duis duis aute sint pariatur aute ex.</p>
+				</div>
+
+
+
+				<div className="square2Y" onClick={() => handleSemesterClick('sem4')}>
+					<h2>Sem 3</h2>
+					<p>Nulla fugiat consectetur do proident fugiat duis duis aute sint pariatur aute ex.</p>
+				</div>
+
+
+				<div className="square2Y" onClick={() => handleSemesterClick('sem4')}>
+					<h2>Sem 4</h2>
+					<p>Nulla fugiat consectetur do proident fugiat duis duis aute sint pariatur aute ex.</p>
+				</div>
+
+			</div>
+
+
+
+			<Footer />
 		</div>
+
 	);
 }
